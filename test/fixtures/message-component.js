bfc440edc0b3871c58ddc23b1f46ab423cf162c3
Gustavo Bastos <me@gustavovinicius.com>
2019-08-13 20:45:00 +0200

    Add specs for component usage

---
+++ test/fixtures/message-component.js
@@ -0,0 +1,4 @@
+<Message
+  id="foo"
+  comment="bar"
+/>
--- test/root-plugin.spec.js
+++ test/root-plugin.spec.js
@@ -3,6 +3,9 @@ const path = require('path');
 const babel = require('@babel/core');
 
 const plugin = (filename) => babel.transformFileSync(path.join(__dirname, 'fixtures', filename), {
+  presets: [
+    '@babel/preset-react',
+  ],
   plugins: [
     [
       path.join(__dirname, '../index.js'),
@@ -20,27 +23,52 @@ const plugin = (filename) => babel.transformFileSync(path.join(__dirname, 'fixtu
 const readPotFile = (filename) => fs.readFileSync(path.join(__dirname, 'gettext/resources/test/fixtures', filename), { encoding: 'utf8' });
 
 describe('Pomes Extract Plugin', () => {
-  it('extract a message call', () => {
-    plugin('message.js');
+  describe('#message', () => {
+    it('extract a message call', () => {
+      plugin('message.js');
 
-    const potFileContent = readPotFile('message.js.pot');
+      const potFileContent = readPotFile('message.js.pot');
 
-    expect(potFileContent.toString()).toMatchSnapshot();
+      expect(potFileContent.toString()).toMatchSnapshot();
+    });
+
+    it('extract a message call with plural form', () => {
+      plugin('message-with-plural.js');
+
+      const potFileContent = readPotFile('message-with-plural.js.pot');
+
+      expect(potFileContent.toString()).toMatchSnapshot();
+    });
+
+    it('merge a message with singular and plural forms', () => {
+      plugin('message-with-singular-and-plural.js');
+
+      const potFileContent = readPotFile('message-with-singular-and-plural.js.pot');
+
+      expect(potFileContent.toString()).toMatchSnapshot();
+    });
+
+    it('raise warning for message without comment', () => {
+      const oldLog = console.log;
+      console.log = jest.fn();
+      plugin('message-without-comment.js');
+
+      const potFileContent = readPotFile('message-without-comment.js.pot');
+
+      expect(potFileContent.toString()).toMatchSnapshot();
+
+      expect(console.log.mock.calls).toMatchSnapshot();
+      console.log = oldLog;
+    });
   });
 
-  it('extract a message call with plural form', () => {
-    plugin('message-with-plural.js');
+  describe('<Message />', () => {
+    it('extract a Message component', () => {
+      plugin('message-component.js');
 
-    const potFileContent = readPotFile('message-with-plural.js.pot');
+      const potFileContent = readPotFile('message-component.js.pot');
 
-    expect(potFileContent.toString()).toMatchSnapshot();
-  });
-
-  it('merge a message with singular and plural forms', () => {
-    plugin('message-with-singular-and-plural.js');
-
-    const potFileContent = readPotFile('message-with-singular-and-plural.js.pot');
-
-    expect(potFileContent.toString()).toMatchSnapshot();
+      expect(potFileContent.toString()).toMatchSnapshot();
+    });
   });
 });
